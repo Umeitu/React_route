3b495df48cc3cd936abf8f374fb98fa66015085c
Umeitu <umeitupelpuo@gmail.com>
2022-04-01 13:50:35 +0000

    added navigate

---
+++ public/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function About() {
+  return (
+    <div>
+    <h1>About me</h1>
+    <div>
+    <p>Mahama was born into a politically active family. His father,
+    Emmanuel Adama Mahama, served as a member of Parliament as well as a regional commissioner in the
+     government of Ghana’s first president, Kwame Nkrumah. John Mahama attended primary school at the 
+     Achimota School in Accra and attended the  Ghana Secondary School in Tamale. He received a bachelor’s 
+     degree in history in 1981 from the University of Ghana, Legon, where he also completed postgraduate 
+     studies in communication in 1986.</p>
+
+    <p>Mahama was born into a politically active family. His father,
+    Emmanuel Adama Mahama, served as a member of Parliament as well as a regional commissioner in the
+     government of Ghana’s first president, Kwame Nkrumah. John Mahama attended primary school at the 
+     Achimota School in Accra and attended the  Ghana Secondary School in Tamale.</p>
+
+    </div>
+    </div>
+  )
+}
+
+export default withLayout(About)
