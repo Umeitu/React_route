05164d3a78891ebebd1fe8675a833947fe469982
Umeitu <umeitupelpuo@gmail.com>
2022-04-01 16:56:45 +0000

    added new data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,46 +3,26 @@ import withLayout from './withLayout'
 
 function Home() {
   return (
- <div>
-   <h1>Welcome To My HomePage</h1> 
     <div>
-    <p> in the reign of his great-grandmother Queen Victoria, 
-    he was named Albert at birth after his great-grandfather Albert,
-     Prince Consort, and was known as "Bertie" to his family and close friends. 
-     His father ascended the throne as King George V in 1910. As the second son of 
-     the king, Albert was not expected to inherit the throne. He spent his early life 
-     in the shadow of his elder brother, Prince Edward, the heir apparent. 
-     Albert attended naval college as a teenager and served in the Royal Navy and Royal Air Force during the First World War. In 1920, he was made Duke of York. He married Lady Elizabeth Bowes-Lyon in 1923, and they had two daughters, Elizabeth and Margaret. In the mid-1920s, he had speech therapy for a stammer, which he learned to manage to some degree. His elder brother ascended the throne as Edward VIII after their father died in 1936, but Edward abdicated later that year to marry the twice-divorced American socialite Wallis Simpson; Albert thereby became the third monarch of the House of Windsor, 
-    taking the regnal name George VI.</p>
-
-    <p> in the reign of his great-grandmother Queen Victoria, 
-    he was named Albert at birth after his great-grandfather Albert,
-     Prince Consort, and was known as "Bertie" to his family and close friends. 
-     His father ascended the throne as King George V in 1910. As the second son of 
-     the king, Albert was not expected to inherit the throne. He spent his early life 
-     in the shadow of his elder brother, Prince Edward, the heir apparent. 
-     Albert attended naval college as a teenager and served in the Royal Navy and Royal Air Force during the First World War. In 1920, he was made Duke of York. He married Lady Elizabeth Bowes-Lyon in 1923, and they had two daughters, Elizabeth and Margaret. In the mid-1920s, he had speech therapy for a stammer, which he learned to manage to some degree. His elder brother ascended the throne as Edward VIII after their father died in 1936, but Edward abdicated later that year to marry the twice-divorced American socialite Wallis Simpson; Albert thereby became the third monarch of the House of Windsor, 
-    taking the regnal name George VI.</p>
-
-    <p> in the reign of his great-grandmother Queen Victoria, 
-    he was named Albert at birth after his great-grandfather Albert,
-     Prince Consort, and was known as "Bertie" to his family and close friends. 
-     His father ascended the throne as King George V in 1910. As the second son of 
-     the king, Albert was not expected to inherit the throne. He spent his early life 
-     in the shadow of his elder brother, Prince Edward, the heir apparent. 
-     Albert attended naval college as a teenager and served in the Royal Navy and Royal Air Force during the First World War. In 1920, he was made Duke of York. He married Lady Elizabeth Bowes-Lyon in 1923, and they had two daughters, Elizabeth and Margaret. In the mid-1920s, he had speech therapy for a stammer, which he learned to manage to some degree. His elder brother ascended the throne as Edward VIII after their father died in 1936, but Edward abdicated later that year to marry the twice-divorced American socialite Wallis Simpson; Albert thereby became the third monarch of the House of Windsor, 
-    taking the regnal name George VI.</p>
+    <h1>Welcome to my HomePage</h1>
+    <div>
+    <p>A home, or domicile, is a space used as a permanent or semi-permanent 
+    residence for one or many humans. It is a fully or
+     semi sheltered space and can have both interior and
+      exterior aspects to it. Homes provide sheltered spaces for instance rooms, where domestic activity can be performed such as sleeping, preparing food, eating and hygiene as well as providing spaces for work and leisure such as remote working, studying and playing.</p>
     
+     <p>A home, or domicile, is a space used as a
+      permanent or semi-permanent residence for one or 
+      many humans. It is a fully or semi sheltered space and can have both interior and exterior aspects to it. Homes provide sheltered spaces for instance rooms, where domestic activity can be performed such as sleeping, preparing food, eating and hygiene as well as providing spaces for work and 
+     leisure such as remote working, studying and playing.</p>
     
-    
-    
-    
-    
+     <p>A home, or domicile, is a space used as a permanent or semi-permanent
+      residence for one or many humans. It is a fully or semi sheltered space 
+      and can have both interior and exterior aspects to it. Homes provide sheltered spaces for instance rooms, where domestic activity can be performed such as sleeping, preparing food, eating and hygiene as well as providing spaces
+      for work and leisure such as remote working, studying and playing.</p>
     </div>
     
-    
-    
-</div>
+    </div>
   )
 }
 
